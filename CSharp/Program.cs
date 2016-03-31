aa53ba2afb46157e61c5b65cb8107ccb80bbbdba
Andrzej Ołdak <andrzej.oldak@hotmail.com>
2016-03-31 17:40:14 +0200

    inheritance and extension methods

---
+++ CSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal fish = new Fish();
+            Console.WriteLine(fish.Breathe());
+            Console.WriteLine(fish.Eat());
+            Console.WriteLine(fish.Sleep());
+            //Console.WriteLine(fish.Swim());
+            Fish fish2 = new Fish();
+            Console.WriteLine(fish2.Breathe());
+            Console.WriteLine(fish2.Eat());
+            Console.WriteLine(fish2.Sleep());
+            Console.WriteLine(fish2.Swim());
+        }
+    }
+
+    class Animal
+    {
+        public virtual string Breathe() { return "Animal::Breathe"; }
+        public virtual string Eat() { return "Animal::Eat"; }
+        public virtual string Sleep() { return "Animal::Sleep"; }
+        public Animal()
+        {
+            Console.WriteLine("animal constructor");
+
+        }
+    }
+
+    class Fish : Animal
+    {
+        public string Breathe() { return "fish::Breathe"; }
+        public override string Eat() { return "fish::Eat"; }
+        public new string Sleep() { return "fish::Sleep"; }
+        public string Swim() { return "fish::Swim"; }
+        public Fish()
+        {
+            Console.WriteLine("fish constructor");
+
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                WriteLine(arg.MakePretty());
+            }
+        }
+    }
+
+    public static class MyExtensionMethods
+    {
+        public static string MakePretty(this string text)
+        {
+            return $"-={text}=-";
+        }
+    }
+}
