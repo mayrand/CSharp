275ce5cbb924b0ff01ed700bf54e02278e548a45
Andrzej Ołdak <andrzej.oldak@gmail.com>
2025-02-01 12:07:36 +0100

    Nullable reference types

---
+++ Nullable reference types/ProductDescription.cs
@@ -0,0 +1,45 @@
+﻿namespace Nullable_reference_types;
+
+public class ProductDescription
+{
+    private string shortDescription;
+    private string? detailedDescription;
+
+    public ProductDescription() // Warning! shortDescription not initialized.
+    {
+    }
+
+    public ProductDescription(string productDescription) =>
+        this.shortDescription = productDescription;
+
+    public void SetDescriptions(string productDescription, string? details=null)
+    {
+        shortDescription = productDescription;
+        detailedDescription = details;
+    }
+
+    public string GetDescription()
+    {
+        if (detailedDescription.Length == 0) // Warning! dereference possible null
+        {
+            return shortDescription;
+        }
+        else
+        {
+            return $"{shortDescription}\n{detailedDescription}";
+        }
+    }
+
+    public string FullDescription()
+    {
+        if (detailedDescription == null)
+        {
+            return shortDescription;
+        }
+        else if (detailedDescription.Length > 0) // OK, detailedDescription can't be null.
+        {
+            return $"{shortDescription}\n{detailedDescription}";
+        }
+        return shortDescription;
+    }
+}
---
+++ Nullable reference types/Program.cs
@@ -0,0 +1,10 @@
+﻿using Nullable_reference_types;
+
+string shortDescription = default; // Warning! non-nullable set to null;
+var product = new ProductDescription(shortDescription); // Warning! static analysis knows shortDescription maybe null.
+
+string description = "widget";
+var item = new ProductDescription(description);
+
+item.SetDescriptions(description, "These widgets will do everything.");
+Console.WriteLine("Hello, World!");
