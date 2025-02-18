828876773acbe364698f64391c862040a220904e
Andrzej Ołdak <andrzej.oldak@gmail.com>
2025-02-18 20:32:00 +0100

    AccessPrivateVarFromObject

---
+++ AccessPrivateVarFromObject/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var user = new User("Fura");
+
+User.SayHello(user);
+public class User {
+    public static void SayHello(User user) {
+        Console.WriteLine($"Hello my name is {user._name}");
+    }
+
+    private string _name;
+    public User(string name) {
+        _name = name;
+    }
+}
