d3db076cdd3fde4003d364d8e0f30f8b9b78885d
andrzejoldak <andrzej.oldak@hotmail.com>
2026-01-03 07:21:29 +0100

    Spans

---
+++ Spans/Program.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+Span<byte> bytes = stackalloc byte[2]; // Using C# 7.2 stackalloc support for spans
+bytes[0] = 42;
+bytes[1] = 43;
+Assert.Equal(42, bytes[0]);
+Assert.Equal(43, bytes[1]);
+bytes[2] = 44; // throws IndexOutOfRangeException
+Console.WriteLine("Hello, World!");
